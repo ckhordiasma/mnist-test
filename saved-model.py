be2394f7ddef781998fcbaf7d38e1a070e6c23da
Christopher Kodama <christopher.kodama.1@us.af.mil>
2019-12-06 14:34:02 -0600

    added matplotlib

--- saved-model.py
+++ saved-model.py
@@ -3,12 +3,16 @@ from PIL import Image
 import PIL.ImageOps
 import tensorflow as tf
 import glob
-
+import matplotlib.pyplot as plt
 modelLoaded = tf.keras.models.load_model('mnistconv.h5')
 np.set_printoptions(linewidth=500,precision=2)
 
+classes = ['zero','one','two','three','four','five','six','seven','eight','nine']
 
 filenames = glob.glob('./handwritten/*.png')
+
+plt.figure(figsize=(4,4))
+
 images = np.empty((len(filenames),28,28,1))
 for i,filename in enumerate(filenames):
     pilImage = Image.open(filename).convert('L')
@@ -16,9 +20,13 @@ for i,filename in enumerate(filenames):
 
     image = np.array(pilImage)
     #print(image)
-
-    image = image / 255.0
+    plt.subplot(4,4,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(image, cmap=plt.cm.binary)
     images[i,:,:] = image[...,tf.newaxis]
+
 
 
 
@@ -28,7 +36,11 @@ print(result)
 ans = tf.argmax(result,1)
 
 print(ans)
+for i,label in enumerate(list(map(lambda x:classes[x],ans))):
+    plt.subplot(4,4,i+1)
+    plt.xlabel(label)
 
+plt.show()
 
 #width, height = image.size
 
